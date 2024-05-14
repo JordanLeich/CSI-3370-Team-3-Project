660fe5363869cc7dfc037d8efeaef5e069fa394a
Jordan Leich <jordanleich@gmail.com>
2024-05-13 20:46:27 -0400

    small improvements

--- mms/src/main/java/com/example/manageMediaController.java
+++ mms/src/main/java/com/example/manageMediaController.java
@@ -1,5 +1,18 @@
 package com.example;
 
+import java.io.IOException;
+
+import javafx.fxml.FXML;
+import javafx.scene.input.MouseEvent;
+
 public class manageMediaController {
-    
+    @FXML
+    void goToMainMenu(MouseEvent event) {
+        try {
+            App.setRoot("menu");
+        } catch (IOException e) {
+            
+            e.printStackTrace();
+        }
+    }
 }
