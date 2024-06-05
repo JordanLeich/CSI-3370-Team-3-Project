90cbfeaa7933cd9e79e7481e0fa2d12a776a2bc8
BryceLucas <147726637+BryceLucas@users.noreply.github.com>
2024-06-05 13:55:43 -0400

    Add files via upload

--- mms/src/main/java/module-info.java
+++ mms/src/main/java/module-info.java
@@ -2,7 +2,7 @@ module com.example {
     requires javafx.controls;
     requires javafx.fxml;
     requires javafx.media;
-    requires javafx.graphics;
+    requires transitive javafx.graphics;
     requires java.desktop;
 
     opens com.example to javafx.fxml;
