af05218eee25dfbe97bbd21b25f9e73c613ddeeb
BryceLucas <147726637+BryceLucas@users.noreply.github.com>
2024-06-05 13:54:23 -0400

    Add files via upload

--- mms/src/main/java/com/example/Song.java
+++ mms/src/main/java/com/example/Song.java
@@ -4,12 +4,12 @@ public class Song {
     private String title;
     private String artist;
     private String filePath;
-    
+
     public Song(String title, String artist, String filePath) {
         this.title = title;
         this.artist = artist;
         this.filePath = filePath;
-     }
+    }
 
     public String getTitle() {
         return title;
